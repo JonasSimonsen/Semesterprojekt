5f0e5ffdae09e37c1e89717072d05c61c62caa18
ERPedersen <emil0srp@gmail.com>
2015-04-07 12:47:02 +0200

    test2

--- Group_4_2sem/src/java/control/control_user.java
+++ Group_4_2sem/src/java/control/control_user.java
@@ -3,14 +3,16 @@
 package control;
 
 import interfaces.interface_user;
-import entity.external_user;
+import entity.User;
+import java.sql.SQLException;
 
 
 public class control_user implements interface_user {
 
     @Override
     public void create_User(int partnerId, String userName, String passWord, String firstName, String lastName, String email, int phoneNum, String organization, int type) {
-        external_user ext_user = new external_user(partnerId, userName, passWord, firstName, lastName, email, phoneNum, organization, type);
+        User ext_user = new User(partnerId, userName, passWord, firstName, lastName, email, phoneNum, organization, type);
+        
     }
 
     
