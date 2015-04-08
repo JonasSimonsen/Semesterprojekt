335be4e972efd009343b8ba4d3f86625db1b6e18
Sander <Sander@192.168.1.43>
2015-04-08 15:15:51 +0200

    Merge origin/master
    Conflicts:
    	Group_4_2sem/nbproject/build-impl.xml
    	Group_4_2sem/nbproject/genfiles.properties
    	Group_4_2sem/nbproject/project.properties
    	Group_4_2sem/nbproject/project.xml

--- Group_4_2sem/src/java/entity/Campaign.java
+++ Group_4_2sem/src/java/entity/Campaign.java
@@ -17,17 +17,17 @@ public class Campaign {
     String description;
     String audience;
     String currency;
-    int cost;
+    double cost;
     int mdfBudget;
     String status;
-    String Quarter;
+    String quarter;
     String startDate;
     String endDate;
     String objective;
     String poeRequirement;
     
 
-    public Campaign(int planNumber, int partnerNumber, String country, String description, String audience, String currency, int cost, int mdfBudget, String status, String Quarter, String startDate, String endDate, String objective, String poeRequirement) {
+    public Campaign(int planNumber, int partnerNumber, String country, String description, String audience, String currency, double cost, int mdfBudget, String status, String quarter, String startDate, String endDate, String objective, String poeRequirement) {
         this.planNumber = planNumber;
         this.partnerNumber = partnerNumber;
         this.country = country;
@@ -37,7 +37,7 @@ public class Campaign {
         this.cost = cost;
         this.mdfBudget = mdfBudget;
         this.status = status;
-        this.Quarter = Quarter;
+        this.quarter = quarter;
         this.startDate = startDate;
         this.endDate = endDate;
         this.objective = objective;
