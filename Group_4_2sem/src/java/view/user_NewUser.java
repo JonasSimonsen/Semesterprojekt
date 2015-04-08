1ea17d4b0ba70a0b301f411add6644494a56cf8a
ERPedersen <emil0srp@gmail.com>
2015-04-08 12:32:30 +0200

    test2

--- Group_4_2sem/src/java/view/user_NewUser.java
+++ Group_4_2sem/src/java/view/user_NewUser.java
@@ -14,13 +14,13 @@ import java.sql.SQLException;
 @WebServlet(name = "user_NewUser", urlPatterns = {"/user_NewUser"})
 public class user_NewUser extends HttpServlet {
 
-    String tempUsername;
-    String tempPassword;
-    String tempFirstName;
-    String tempLastName;
-    String tempEmail;
-    int tempPhoneNumber;
-    String tempOrganization;
+    private String tempUsername;
+    private String tempPassword;
+    private String tempFirstName;
+    private String tempLastName;
+    private String tempEmail;
+    private int tempPhoneNumber;
+    private String tempOrganization;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
