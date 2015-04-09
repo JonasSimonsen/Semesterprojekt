b25789147799661bd772845f449c582eb92ff088
ZARDOZ <ZARDOZ@ZARDOZ-PC.lan>
2015-04-09 11:51:23 +0200

    Oprettet test class

---
+++ Group_4_2sem/test/DatabaseInputTests.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import interfaces.*;
+import control.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ZARDOZ
+ */
+public class DatabaseInputTests {
+    
+    private interface_campaign campInterface;
+    private interface_user userInterface;
+    
+    public DatabaseInputTests() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testUserEntry(){
+        
+    }
+    
+    @Test
+    public void testCampaignEntry(){
+    
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
