011a72cad1222fc498a1620322ff036da888a661
jonassimonsen <jonassimonsen@192.168.87.128>
2015-04-10 11:43:18 +0200

    Small changes

---
+++ Group_4_2sem/src/java/view/campaign_ViewCampaign.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import data.DatabaseInfo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.persistence.Table;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jonassimonsen
+ */
+@WebServlet(name = "campaign_ViewCampaign", urlPatterns = {"/campaign_ViewCampaign"})
+public class campaign_ViewCampaign extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        PrintWriter out = response.getWriter();
+        String planno=request.getParameter("Planno");
+        try {
+            Class.forName(DatabaseInfo.driver);
+                     Connection con=DriverManager.getConnection(DatabaseInfo.URL, DatabaseInfo.ID, DatabaseInfo.PW);               
+                     PreparedStatement ps=con.prepareStatement("select * from PARTNERPLAN where PLANNO=?");
+                     
+                     
+                     
+                    RequestDispatcher rd = request.getRequestDispatcher("campaigns_viewspecific.jsp");
+                    rd.forward(request, response);
+ 
+              }catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+                }
+       }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
