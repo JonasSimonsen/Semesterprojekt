9b7a023e614328d9f0c549bda9cdc4ea6fd668fc
jonassimonsen <jonassimonsen@10.50.137.225>
2015-04-13 12:07:44 +0200

    First Commit

--- Group_4_2sem/src/java/view/campaign_NewCampaign.java
+++ Group_4_2sem/src/java/view/campaign_NewCampaign.java
@@ -16,6 +16,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Campaign2;
 
 /**
  *
@@ -25,6 +26,8 @@ import javax.servlet.http.HttpServletResponse;
 public class campaign_NewCampaign extends HttpServlet {
     
     
+    private int tempPlanNumber;
+    private String tempSubmitDate;
     private String tempContactName;
     private String tempCompanyName;
     private String tempAddress;
@@ -43,6 +46,7 @@ public class campaign_NewCampaign extends HttpServlet {
     private int tempMultitouchCampaign;
     private int tempDoorOpenerCampaign;
     private int tempThirdPartyCampaign;
+    private int tempDirectMail;
     private int tempBlitzCampaign;
     private String tempProgramDescription;
     
@@ -96,58 +100,61 @@ public class campaign_NewCampaign extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        tempContactName = request.getParameter("");
-        tempCompanyName = request.getParameter("");
-        tempAddress = request.getParameter("");
-        tempEmail = request.getParameter("");
-        tempPhone = request.getParameter("");
+        tempPlanNumber = Integer.parseInt(request.getParameter("plan_number"));
+        tempSubmitDate = request.getParameter("submit_date");
+        tempContactName = request.getParameter("contact_name");
+        tempCompanyName = request.getParameter("company_name");
+        tempAddress = request.getParameter("company_address");
+        tempEmail = request.getParameter("contact_email");
+        tempPhone = request.getParameter("contact_phone");
     
-        tempProgramDate = request.getParameter("");
-        tempStartTime = request.getParameter("");
-        tempEndTime = request.getParameter("");
-        tempEstimatedOfAttendess = Integer.parseInt(request.getParameter(""));
-        tempVenueName = request.getParameter("");
-        tempVenueAddress = request.getParameter("");
+        tempProgramDate = request.getParameter("program_date");
+        tempStartTime = request.getParameter("start_time");
+        tempEndTime = request.getParameter("end_time");
+        tempEstimatedOfAttendess = Integer.parseInt(request.getParameter("attendum"));
+        tempVenueName = request.getParameter("venue_name");
+        tempVenueAddress = request.getParameter("venue_address");
     
-        tempFaceToFaceEvent = Integer.parseInt(request.getParameter(""));
-        tempTradeshow = Integer.parseInt(request.getParameter(""));
-        tempMultitouchCampaign = Integer.parseInt(request.getParameter(""));
-        tempDoorOpenerCampaign = Integer.parseInt(request.getParameter(""));
-        tempThirdPartyCampaign = Integer.parseInt(request.getParameter(""));
-        tempBlitzCampaign = Integer.parseInt(request.getParameter(""));
-        tempProgramDescription = request.getParameter("");
+        tempFaceToFaceEvent = Integer.parseInt(request.getParameter("face_to_face"));
+        tempTradeshow = Integer.parseInt(request.getParameter("tradeshow"));
+        tempMultitouchCampaign = Integer.parseInt(request.getParameter("multitouch"));
+        tempDoorOpenerCampaign = Integer.parseInt(request.getParameter("door_opener"));
+        tempThirdPartyCampaign = Integer.parseInt(request.getParameter("third_party"));
+        tempDirectMail = Integer.parseInt(request.getParameter("direct_mail"));
+        tempBlitzCampaign = Integer.parseInt(request.getParameter("blitz"));
+        tempProgramDescription = request.getParameter("program_desc");
     
-        tempDellStorageSC4000Series = Integer.parseInt(request.getParameter(""));
-        tempDellStoragePS4210Series = Integer.parseInt(request.getParameter(""));
-        tempDellStorageSolution = Integer.parseInt(request.getParameter(""));
-        tempFlashAtThePriceOfDisk = Integer.parseInt(request.getParameter(""));
-        tempFluidCacheForSAN = Integer.parseInt(request.getParameter(""));
-        tempDateProtection = Integer.parseInt(request.getParameter(""));
-        tempTheLastGenerationOfDellPowerEdgeServers = Integer.parseInt(request.getParameter(""));
-        tempWindowsServer2003Migration = Integer.parseInt(request.getParameter(""));
-        tempX86ServerTransition = Integer.parseInt(request.getParameter(""));
-        tempPowerEdgeVRTX = Integer.parseInt(request.getParameter(""));
-        tempSoftwareDefinedNetworking = Integer.parseInt(request.getParameter(""));
-        tempUserCentricNetworking = Integer.parseInt(request.getParameter(""));
-        tempCloudClientComputing = Integer.parseInt(request.getParameter(""));
-        tempConvergedInfrastructureHardware = Integer.parseInt(request.getParameter(""));
-        tempDellConvergedBladeDataCenter = Integer.parseInt(request.getParameter(""));
-        tempOptimizedEnteprise = Integer.parseInt(request.getParameter(""));
-        tempPowerEdgeFXArchitecture = Integer.parseInt(request.getParameter(""));
-        tempSoftwareDefinedStorage = Integer.parseInt(request.getParameter(""));
-        tempIsThereASoftwareComponentToYourCampaign = request.getParameter("");
+        tempDellStorageSC4000Series = Integer.parseInt(request.getParameter("storage_sc4000"));
+        tempDellStoragePS4210Series = Integer.parseInt(request.getParameter("storage_ps4210"));
+        tempDellStorageSolution = Integer.parseInt(request.getParameter("storage_solutions"));
+        tempFlashAtThePriceOfDisk = Integer.parseInt(request.getParameter("storage_flash"));
+        tempFluidCacheForSAN = Integer.parseInt(request.getParameter("storage_fluidcache"));
+        tempDateProtection = Integer.parseInt(request.getParameter("storage_data_protection"));
+        tempTheLastGenerationOfDellPowerEdgeServers = Integer.parseInt(request.getParameter("server_newgen"));
+        tempWindowsServer2003Migration = Integer.parseInt(request.getParameter("server_win2003"));
+        tempX86ServerTransition = Integer.parseInt(request.getParameter("server_x86"));
+        tempPowerEdgeVRTX = Integer.parseInt(request.getParameter("server_vrtx"));
+        tempSoftwareDefinedNetworking = Integer.parseInt(request.getParameter("networking_sdn"));
+        tempUserCentricNetworking = Integer.parseInt(request.getParameter("networking_user_centric"));
+        tempCloudClientComputing = Integer.parseInt(request.getParameter("solutions_cloud"));
+        tempConvergedInfrastructureHardware = Integer.parseInt(request.getParameter("solutions_converged"));
+        tempDellConvergedBladeDataCenter = Integer.parseInt(request.getParameter("solutions_blade"));
+        tempOptimizedEnteprise = Integer.parseInt(request.getParameter("solutions_futureready"));
+        tempPowerEdgeFXArchitecture = Integer.parseInt(request.getParameter("solutions_poweredge"));
+        tempSoftwareDefinedStorage = Integer.parseInt(request.getParameter("solutions_sds"));
+        tempIsThereASoftwareComponentToYourCampaign = request.getParameter("campaign_component");
     
-        tempSMB = Integer.parseInt(request.getParameter(""));
-        tempLE = Integer.parseInt(request.getParameter(""));
-        tempPUB = Integer.parseInt(request.getParameter(""));
+        tempSMB = Integer.parseInt(request.getParameter("target_smb"));
+        tempLE = Integer.parseInt(request.getParameter("target_le"));
+        tempPUB = Integer.parseInt(request.getParameter("target_pub"));
     
-        tempTotalProjectedCostOfProgram = Double.parseDouble(request.getParameter(""));
-        tempTotalMDFRequestingFromDell = Double.parseDouble(request.getParameter(""));
-        tempPreferredMethodOfReimbursement = request.getParameter("");
-        tempParticipatingTechnologyPartner = request.getParameter("");
-        tempTotalTechnologyPartner = Double.parseDouble(request.getParameter(""));
-        tempEstimatedOfOpportunitues = Integer.parseInt(request.getParameter(""));
-        tempEstimatedRevenueFromProgram = Integer.parseInt(request.getParameter(""));
+        tempTotalProjectedCostOfProgram = Double.parseDouble(request.getParameter("cost"));
+        tempTotalMDFRequestingFromDell = Double.parseDouble(request.getParameter("mdf_req"));
+        tempPreferredMethodOfReimbursement = request.getParameter("reimbursement");
+        tempParticipatingTechnologyPartner = request.getParameter("partners");
+        tempTotalTechnologyPartner = Double.parseDouble(request.getParameter("mdf_contr"));
+        tempEstimatedOfOpportunitues = Integer.parseInt(request.getParameter("opportunities"));
+        tempEstimatedRevenueFromProgram = Integer.parseInt(request.getParameter("est_renevue"));
         
         
 //        tempCountry = request.getParameter("country");
@@ -162,16 +169,11 @@ public class campaign_NewCampaign extends HttpServlet {
 //        tempObjective = request.getParameter("objective");
 
 //        Campaign campaign = new Campaign(tempPlanNumber, tempPartnerNumber, tempCountry, tempDescription, tempAudience, tempCurrency, tempCost, tempMdfBudget, tempStatus, tempQuarter, tempStartDate, tempEndDate, tempObjective, tempPoeRequirement);
-        Campaign campaign = new Campaign(tempContactName, tempCompanyName, tempAddress, tempEmail, tempPhone, tempProgramDate, tempStartTime, tempEndTime, tempEstimatedOfAttendess, tempVenueName, tempVenueAddress, 
-                tempFaceToFaceEvent, tempTradeshow, tempMultitouchCampaign, tempDoorOpenerCampaign, tempThirdPartyCampaign, tempBlitzCampaign, tempProgramDescription,
-        tempDellStorageSC4000Series, tempDellStoragePS4210Series, tempDellStorageSolution, tempFlashAtThePriceOfDisk, tempFluidCacheForSAN, tempDateProtection, tempTheLastGenerationOfDellPowerEdgeServers, tempWindowsServer2003Migration,
-        tempX86ServerTransition, tempPowerEdgeVRTX, tempSoftwareDefinedNetworking, tempUserCentricNetworking, tempCloudClientComputing, tempConvergedInfrastructureHardware, tempDellConvergedBladeDataCenter, tempOptimizedEnteprise, tempPowerEdgeFXArchitecture ,
-        tempSoftwareDefinedStorage, tempIsThereASoftwareComponentToYourCampaign, tempSMB, tempLE, tempPUB, tempTotalProjectedCostOfProgram, tempTotalMDFRequestingFromDell, tempPreferredMethodOfReimbursement, tempParticipatingTechnologyPartner,
-        tempTotalTechnologyPartner, tempEstimatedOfOpportunitues, tempEstimatedRevenueFromProgram)
+        Campaign2 campaign = new Campaign2(tempPlanNumber, tempSubmitDate, tempContactName, tempCompanyName, tempAddress, tempEmail, tempPhone, tempProgramDate, tempStartTime, tempEndTime, tempEstimatedOfAttendess, tempVenueName, tempVenueAddress, tempFaceToFaceEvent, tempTradeshow, tempMultitouchCampaign, tempDoorOpenerCampaign, tempThirdPartyCampaign, tempDirectMail, tempBlitzCampaign, tempProgramDescription, tempDellStorageSC4000Series, tempDellStoragePS4210Series, tempDellStorageSolution, tempFlashAtThePriceOfDisk, tempFluidCacheForSAN, tempDateProtection, tempTheLastGenerationOfDellPowerEdgeServers, tempWindowsServer2003Migration, tempX86ServerTransition, tempPowerEdgeVRTX, tempSoftwareDefinedNetworking, tempUserCentricNetworking, tempCloudClientComputing, tempConvergedInfrastructureHardware, tempDellConvergedBladeDataCenter, tempOptimizedEnteprise, tempPowerEdgeFXArchitecture, tempSoftwareDefinedStorage, tempIsThereASoftwareComponentToYourCampaign, tempSMB, tempLE, tempPUB, tempTotalProjectedCostOfProgram, tempTotalMDFRequestingFromDell, tempPreferredMethodOfReimbursement, tempParticipatingTechnologyPartner, tempTotalTechnologyPartner, tempEstimatedOfOpportunitues, tempEstimatedRevenueFromProgram);
         CampaignDAO cm = new CampaignDAO();
 
         try {
-            cm.submitNewCampaign(campaign);
+            cm.submitNewCampaignV2(campaign);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
