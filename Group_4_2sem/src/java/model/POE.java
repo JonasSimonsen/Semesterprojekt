18ec0161b4756ee16e400344ecf86af90f187c69
ERPedersen <emil0srp@gmail.com>
2015-04-16 10:04:11 +0200

    terms

---
+++ Group_4_2sem/src/java/model/POE.java
@@ -0,0 +1,26 @@
+package model;
+
+
+public class POE {
+    
+    private final String poe_id;
+    private String poe_path;
+
+    public POE(String poe_id, String poe_path) {
+        this.poe_id = poe_id;
+        this.poe_path = poe_path;
+    }
+
+    public String getPoe_path() {
+        return poe_path;
+    }
+
+    public void setPoe_path(String poe_path) {
+        this.poe_path = poe_path;
+    }
+
+    public String getPoe_id() {
+        return poe_id;
+    }
+
+}
