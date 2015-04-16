6a6d1633992aa9fe6957a2e6a075dbb37fc0642c
ERPedersen <emil0srp@gmail.com>
2015-04-16 13:06:20 +0200

    terms

--- Group_4_2sem/src/java/model/Campaign2.java
+++ Group_4_2sem/src/java/model/Campaign2.java
@@ -62,8 +62,9 @@ public class Campaign2 {
     private int opportunities;
     private double est_renevue;
     private String status;
+    private int has_poe;
 
-    public Campaign2(int plan_number, String submit_date, String contact_name, String company_name, String company_address, String contact_email, String contact_phone, String program_date, String start_time, String end_time, int attendum, String venue_name, String venue_address, int face_to_face, int tradeshow, int multitouch, int door_opener, int third_party, int direct_mail, int blitz, String program_desc, int storage_sc4000, int storage_ps4210, int storage_solutions, int storage_flash, int storage_fluidcache, int storage_data_protection, int server_newgen, int server_win2003, int server_x86, int server_vrtx, int networking_sdn, int networking_user_centric, int solutions_cloud, int solutions_converged, int solutions_blade, int solutions_futureready, int solutions_poweredge, int solutions_sds, String campaign_component, int target_smb, int target_le, int target_pub, double cost, double mdf_req, String reimbursement, String partners, double mdf_contr, int opportunities, double est_renevue, String status) {
+    public Campaign2(int plan_number, String submit_date, String contact_name, String company_name, String company_address, String contact_email, String contact_phone, String program_date, String start_time, String end_time, int attendum, String venue_name, String venue_address, int face_to_face, int tradeshow, int multitouch, int door_opener, int third_party, int direct_mail, int blitz, String program_desc, int storage_sc4000, int storage_ps4210, int storage_solutions, int storage_flash, int storage_fluidcache, int storage_data_protection, int server_newgen, int server_win2003, int server_x86, int server_vrtx, int networking_sdn, int networking_user_centric, int solutions_cloud, int solutions_converged, int solutions_blade, int solutions_futureready, int solutions_poweredge, int solutions_sds, String campaign_component, int target_smb, int target_le, int target_pub, double cost, double mdf_req, String reimbursement, String partners, double mdf_contr, int opportunities, double est_renevue, String status, int has_poe) {
         this.plan_number = plan_number;
         this.submit_date = submit_date;
         this.contact_name = contact_name;
@@ -115,6 +116,7 @@ public class Campaign2 {
         this.opportunities = opportunities;
         this.est_renevue = est_renevue;
         this.status = status;
+        this.has_poe = has_poe;
     }
 
     public int getPlan_number() {
@@ -524,6 +526,15 @@ public class Campaign2 {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public int getHas_poe() {
+        return has_poe;
+    }
+
+    public void setHas_poe(int has_poe) {
+        this.has_poe = has_poe;
+    }
+    
     
     
     
