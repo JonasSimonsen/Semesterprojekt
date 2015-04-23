2411d85191e1f224b4957fb9f059c666f1545cd4
ZARDOZ <ZARDOZ@ZARDOZ-PC.lan>
2015-04-23 09:11:33 +0200

    Refactor control til data

---
+++ Group_4_2sem/src/java/data/ActivityDAO.java
@@ -0,0 +1,52 @@
+package data;
+
+import interfaces.Interface_ActivityDAO;
+import model.Activity;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ActivityDAO implements Interface_ActivityDAO {
+    @Override
+    public void createNewActivity(Activity activity) throws SQLException, ClassNotFoundException{
+        Statement statement = null;
+        Connection connection = null;
+        int ID = activity.getID();
+        int PLANNO = activity.getPlanNo();
+        String COMMENTS = activity.getComments();
+        String COMMENTDATE = activity.getDate();                                // Variabler som henter values fra Activity objektet passeret igennem servletten.
+        try {
+            Class.forName(DatabaseInfo.driver);                                 // Henter database driveren.
+            connection = DriverManager.getConnection(DatabaseInfo.URL, DatabaseInfo.ID, DatabaseInfo.PW);  // Opretter forbindelse til databasen med info fra DB klassen
+            statement = connection.createStatement();                           // Opretter forbindelse til til databasen for statement
+            String query = "INSERT INTO PARTNERS (COMMENTS,PLANNO,COMMENTDATE,ID) VALUES ('" + COMMENTS + "'," + PLANNO + ",'" + COMMENTDATE + "'," + ID + ")";  // Indsætter en ny partner i PARTNERS table i databasen
+            statement.executeQuery(query);                                      // Sender queryen ind i databasen
+        } finally {  
+            statement.close();                                                  // Lukker forbindelsen til databasen
+            connection.close();
+        }
+    }
+    public static ArrayList<Activity> getActivity() throws SQLException, ClassNotFoundException{
+        ArrayList<Activity> getActivity = new ArrayList();                      // Opretter Arraylist til at indeholde Activity objekter
+        ResultSet rs = null;
+        Statement statement = null;
+        Connection connection = null;
+        try {
+            Class.forName(DatabaseInfo.driver);                                 // Henter database driveren.
+            connection = DriverManager.getConnection(DatabaseInfo.URL, DatabaseInfo.ID, DatabaseInfo.PW);  // Opretter forbindelse til databasen med info fra DB klassen
+            statement = connection.createStatement();                           // Opretter forbindelse til til databasen for statement
+            String query = "SELECT * FROM PARTNERPLAN";                         // Vælger alle informationer i PARTNERPLAN table
+            rs = statement.executeQuery(query);
+            while (rs.next()) {
+                getActivity.add(new Activity(rs.getString("COMMENTS"),rs.getInt("PLANNO"),rs.getString("COMMENTDATE"),rs.getInt("ID"))); // Tilføjer Activity objekter til Arraylisten
+            }
+        } finally {
+            statement.close();                                                  // Lukker forbindelsen til databasen
+            connection.close();
+        }
+        return getActivity;                                                     // Returnerer Arraylisten med alle Activity objekterne igennem metoden
+    }
+}
---
+++ Group_4_2sem/src/java/data/DatabaseInfo.java
@@ -0,0 +1,11 @@
+package data;
+
+import java.sql.Connection;
+
+public class DatabaseInfo {
+
+    public static String driver = "oracle.jdbc.driver.OracleDriver";
+    public static String URL = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+    public static String ID = "cphnb98";
+    public static String PW = "cphnb98";
+}
