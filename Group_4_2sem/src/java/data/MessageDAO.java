1e5a3ac78f4eed9aa45a299073dc5adedb9d64ab
ZARDOZ <ZARDOZ@ZARDOZ-PC.lan>
2015-04-24 12:28:30 +0200

    Refactors are hell

--- Group_4_2sem/src/java/data/MessageDAO.java
+++ Group_4_2sem/src/java/data/MessageDAO.java
@@ -10,12 +10,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MessageDAO implements Interface_MessageDAO {
-    
+
+    private ArrayList<Message> getMessages = new ArrayList();                      // Opretter Arraylist til at indeholde Message objekter
+
     @Override
-    public void createNewMessage(Message message) throws SQLException, ClassNotFoundException{
+    public void createNewMessage(Message message) throws SQLException, ClassNotFoundException {
         PreparedStatement prep = null;
         Connection connection = null;
-        
+
         try {
             Class.forName(DatabaseInfo.driver);                                 // Henter database driveren.
             connection = DriverManager.getConnection(DatabaseInfo.URL, DatabaseInfo.ID, DatabaseInfo.PW);  // Opretter forbindelse til databasen med info fra DB klassen
@@ -27,15 +29,15 @@ public class MessageDAO implements Interface_MessageDAO {
             prep.setInt(4, message.getID());
             prep.setString(5, message.getUsername());
             prep.executeQuery();                                      // Sender queryen ind i databasen
-        } finally {  
+        } finally {
             prep.close();                                                  // Lukker forbindelsen til databasen
             connection.close();
         }
     }
-    
+
     @Override
-    public ArrayList<Message> getMessages() throws SQLException, ClassNotFoundException{
-        ArrayList<Message> getMessages = new ArrayList();                      // Opretter Arraylist til at indeholde Message objekter
+    public ArrayList<Message> getMessages() throws SQLException, ClassNotFoundException {
+        getMessages = new ArrayList();                      // Opretter Arraylist til at indeholde Message objekter
         ResultSet rs = null;
         PreparedStatement prep = null;
         Connection connection = null;
@@ -46,7 +48,7 @@ public class MessageDAO implements Interface_MessageDAO {
             prep = connection.prepareStatement(query);                          // Opretter forbindelse til til databasen for statement
             rs = prep.executeQuery();
             while (rs.next()) {
-                getMessages.add(new Message(rs.getInt("COMMENTID"),rs.getString("COMMENTS"),rs.getInt("CAMPNO"),rs.getString("COMMENTDATE"),rs.getInt("ACT_ID"),rs.getString("USERNAME"))); // Tilføjer Message objekter til Arraylisten
+                getMessages.add(new Message(rs.getInt("COMMENTID"), rs.getString("COMMENTS"), rs.getInt("CAMPNO"), rs.getString("COMMENTDATE"), rs.getInt("ACT_ID"), rs.getString("USERNAME"))); // Tilføjer Message objekter til Arraylisten
             }
         } finally {
             prep.close();                                                       // Lukker forbindelsen til databasen
@@ -54,13 +56,15 @@ public class MessageDAO implements Interface_MessageDAO {
         }
         return getMessages;                                                     // Returnerer Arraylisten med alle Message objekterne igennem metoden
     }
-    
+
     @Override
-    public ArrayList<Message> getSpecificMessage(int ID) throws SQLException, ClassNotFoundException{
-        ArrayList<Message> getMessages = new ArrayList();                      // Opretter Arraylist til at indeholde Message objekter
+    public ArrayList<Message> getSpecificMessage(int ID) throws SQLException, ClassNotFoundException {
+        getMessages = new ArrayList();                      // Opretter Arraylist til at indeholde Message objekter
         ResultSet rs = null;
         PreparedStatement prep = null;
         Connection connection = null;
+        int test = 0;
+        System.out.println("what is going on");
         try {
             Class.forName(DatabaseInfo.driver);                                 // Henter database driveren.
             connection = DriverManager.getConnection(DatabaseInfo.URL, DatabaseInfo.ID, DatabaseInfo.PW);  // Opretter forbindelse til databasen med info fra DB klassen
@@ -69,7 +73,9 @@ public class MessageDAO implements Interface_MessageDAO {
             prep.setInt(1, ID);
             rs = prep.executeQuery();
             while (rs.next()) {
-                getMessages.add(new Message(rs.getInt("COMMENTID"),rs.getString("COMMENTS"),rs.getInt("CAMPNO"),rs.getString("COMMENTDATE"),rs.getInt("ACT_ID"),rs.getString("USERNAME"))); // Tilføjer Message objekter til Arraylisten
+                System.out.println("Test: " + rs.getInt("ACT_ID") + "While run number: " + test);
+                test++;
+                getMessages.add(new Message(rs.getInt("COMMENTID"), rs.getString("COMMENTS"), rs.getInt("CAMPNO"), rs.getString("COMMENTDATE"), rs.getInt("ACT_ID"), rs.getString("USERNAME"))); // Tilføjer Message objekter til Arraylisten
             }
         } finally {
             prep.close();                                                  // Lukker forbindelsen til databasen
