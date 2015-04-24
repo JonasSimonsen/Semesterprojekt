7f7f55571113cdaf68166af52a9a5088220e2340
ZARDOZ <ZARDOZ@ZARDOZ-PC.lan>
2015-04-24 12:02:17 +0200

    Refactors are hell

--- Group_4_2sem/src/java/data/MessageDAO.java
+++ Group_4_2sem/src/java/data/MessageDAO.java
@@ -19,12 +19,13 @@ public class MessageDAO implements Interface_MessageDAO {
         try {
             Class.forName(DatabaseInfo.driver);                                 // Henter database driveren.
             connection = DriverManager.getConnection(DatabaseInfo.URL, DatabaseInfo.ID, DatabaseInfo.PW);  // Opretter forbindelse til databasen med info fra DB klassen
-            String query = "INSERT INTO MESSAGES (COMMENTID,COMMENTS,CAMPNO,COMMENTDATE,ACT_ID) VALUES (act_seq.nextval,?,(SELECT CAMPNO FROM CAMPAIGN WHERE CAMPNO=?),?,(SELECT ID FROM USERS WHERE ID=?))";  // Indsætter en ny partner i PARTNERS table i databasen
+            String query = "INSERT INTO MESSAGES (COMMENTID,COMMENTS,CAMPNO,COMMENTDATE,ACT_ID,USERNAME) VALUES (act_seq.nextval,?,(SELECT CAMPNO FROM CAMPAIGN WHERE CAMPNO=?),?,(SELECT ID FROM USERS WHERE ID=?),?)";  // Indsætter en ny partner i PARTNERS table i databasen
             prep = connection.prepareStatement(query);                           // Opretter forbindelse til til databasen for statement
             prep.setString(1, message.getComments());
             prep.setInt(2, message.getCampNum());
             prep.setString(3, message.getDate());
             prep.setInt(4, message.getID());
+            prep.setString(5, message.getUsername());
             prep.executeQuery();                                      // Sender queryen ind i databasen
         } finally {  
             prep.close();                                                  // Lukker forbindelsen til databasen
@@ -45,7 +46,7 @@ public class MessageDAO implements Interface_MessageDAO {
             prep = connection.prepareStatement(query);                          // Opretter forbindelse til til databasen for statement
             rs = prep.executeQuery();
             while (rs.next()) {
-                getMessages.add(new Message(rs.getInt("COMMENTID"),rs.getString("COMMENTS"),rs.getInt("CAMPNO"),rs.getString("COMMENTDATE"),rs.getInt("ACT_ID"))); // Tilføjer Message objekter til Arraylisten
+                getMessages.add(new Message(rs.getInt("COMMENTID"),rs.getString("COMMENTS"),rs.getInt("CAMPNO"),rs.getString("COMMENTDATE"),rs.getInt("ACT_ID"),rs.getString("USERNAME"))); // Tilføjer Message objekter til Arraylisten
             }
         } finally {
             prep.close();                                                       // Lukker forbindelsen til databasen
@@ -68,7 +69,7 @@ public class MessageDAO implements Interface_MessageDAO {
             prep.setInt(1, ID);
             rs = prep.executeQuery();
             while (rs.next()) {
-                getMessages.add(new Message(rs.getInt("COMMENTID"),rs.getString("COMMENTS"),rs.getInt("CAMPNO"),rs.getString("COMMENTDATE"),rs.getInt("ACT_ID"))); // Tilføjer Message objekter til Arraylisten
+                getMessages.add(new Message(rs.getInt("COMMENTID"),rs.getString("COMMENTS"),rs.getInt("CAMPNO"),rs.getString("COMMENTDATE"),rs.getInt("ACT_ID"),rs.getString("USERNAME"))); // Tilføjer Message objekter til Arraylisten
             }
         } finally {
             prep.close();                                                  // Lukker forbindelsen til databasen
